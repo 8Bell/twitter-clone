d575b4ddfb89b109b7af258eb74d268a767449e7
Mori <dev@8bell.com>
2022-03-16 17:44:41 +0900

    twitter clone 5.1

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,31 +1,45 @@
 import { authService, dbService } from 'fbase';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 const Profile = ({ userObj }) => {
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
 
     const history = useHistory();
 const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
 };
-const getMyTwits = async() => {
- const twits = await dbService
- .collection('twits')
- .where('creatorId', '==', userObj.uid )
- .orderBy('createdAt')
- .get();
- console.log('doc',twits.docs.map((doc) => doc.data()))
+// const getMyTwits = async() => {
+//  const twits = await dbService
+//  .collection('twits')
+//  .where('creatorId', '==', userObj.uid )
+//  .orderBy('createdAt')
+//  .get();
+//  console.log('doc',twits.docs.map((doc) => doc.data()))
+// }
+
+// useEffect(()=>{
+// getMyTwits();
+// },[])
+const onChange = (e) => {
+    setNewDisplayName(e.target.value)
 }
-
-useEffect(()=>{
-getMyTwits();
-},[])
-
+const onSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+        await userObj.updateProfile({
+            displayName: newDisplayName,
+        });
+    }
+};
 return(
-    <div>
-<span>Profile</span>
+    <>
+<form onSubmit={onSubmit}>
+    <input onChange={onChange} type='text' placeholder='Edit your name' value={newDisplayName}/>
+    <input type='submit' value='Update'/>
+</form>
 <button onClick={onLogOutClick}>Log Out</button>
-    </div>
+    </>
 )
 }
 export default Profile;
